8fb9d5f5c0268616237aefc774575c8d3b78539c
i528376 <v.som.sekhar@sap.com>
2024-12-01 11:14:37 +0530

    SpringBoot Web Starter State

---
+++ SpringBoot-Web/src/main/java/com/vsomsekhar/springbootweb/SpringBoot/Web/SpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.vsomsekhar.springbootweb.SpringBoot.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-Web/src/test/java/com/vsomsekhar/springbootweb/SpringBoot/Web/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsomsekhar.springbootweb.SpringBoot.Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
